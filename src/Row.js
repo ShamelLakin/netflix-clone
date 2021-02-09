84c7f203ed4d1e95ef03da41970cdb38b5a7a762
Shamel Lakin <christlife4u@gmail.com>
2021-02-09 17:48:56 -0500

    rendering images

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 
+const base_url = "https://image.tmdb.org/t/p/original/"
+
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
 
@@ -8,16 +10,27 @@ function Row({ title, fetchUrl }) {
     //   empty bracket below = run once and don't run agin
     async function fetchData() {
       const request = await axios.get(fetchUrl);
-      console.log(request);
+      setMovies(request.data.results);
       return request;
     }
     fetchData();
-  }, []);
+  }, [fetchUrl]);
+
+//   console.log[movies];
 
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
 
+      <div classname="row__posters">
+        {/* { row_posters} */}
+        {movies.map((movie) => (
+          <img
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
       {/* container -> posters */}
     </div>
   );
