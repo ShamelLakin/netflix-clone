738d09519ab1c9ac86f2df1f2f14bff7120ac611
Shamel Lakin <christlife4u@gmail.com>
2021-02-09 19:00:54 -0500

    added movies rows

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
-import "./Row.css"
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -24,9 +24,9 @@ function Row({ title, fetchUrl }) {
       <h2>{title}</h2>
 
       <div className="row__posters">
-        {/* { row_posters} */}
         {movies.map((movie) => (
           <img
+            key={movie.id}
             className="row_poster"
             src={`${base_url}${movie.poster_path}`}
             alt={movie.name}
